fb677953ec741e44433cc1d37454a93fe0a26248
inyoung4121 <inyoungyang01@gmail.com>
2024-10-02 13:05:14 +0900

    first commit

---
+++ src/test/java/in/grpc_jwtauth_server/GRpcJwTauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.grpc_jwtauth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcJwTauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
